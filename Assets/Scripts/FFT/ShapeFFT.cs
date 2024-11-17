25344ffc66cdc556fe00050e477d19900ce5683c
Lingheng Tao <40527691+Lockbrains@users.noreply.github.com>
2024-11-16 23:18:47 -0500

    FFT Compute - Error

--- Assets/Scripts/FFT/ShapeFFT.cs
+++ Assets/Scripts/FFT/ShapeFFT.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using Random = UnityEngine.Random;
+using Vector2 = UnityEngine.Vector2;
 
 /// <summary>
 /// For generating the Wave Shape using FFT.
@@ -11,28 +13,110 @@ public class ShapeFFT : MonoBehaviour
 {
     [SerializeField] private int resolution = 128;
     [SerializeField] private float frequency = 5f;
+    [SerializeField] private float amplitude = 1f;
+    [SerializeField] private float speed = 1.0f;
 
     [Header("For Spectrum Visualization")]
     [SerializeField] private MeshRenderer spectrumPlaneMR;
+    
+    [Header("Phillips Spectrum")]
+    [SerializeField] private float windSpeed = 10f; // wind speed
+    [SerializeField] private float g = 9.81f;       // gravity accelaration
+    [SerializeField] private float A = 0.01f;       // adjustment factor
      
     private float[,] spectrum;
+    private Complex[,] fftOutput;
     private int spectrumSize;
     private Texture2D spectrumTexture;
+    
+    [SerializeField] private ComputeShader computeShader;
+    private int kernelHandle;
+    private int kernelInitSpectrum;
+    private int kernelUpdatePhase;
+    private int kernelFFT1D_Horizontal;
+    private int kernelFFT1D_Vertical;
+    private int kernelIFFT1D_Horizontal;
+    private int kernelIFFT1D_Vertical;
+    private int kernelFinalOutput;
+
+    private RenderTexture tempBuffer;
+    private RenderTexture heightMap;
+    
+    private int threadGroupX, threadGroupY;
+
+    void Awake()
+    {
+        threadGroupX = Mathf.CeilToInt((float)resolution / 8);
+        threadGroupY = Mathf.CeilToInt((float)resolution / 8);
+        
+        kernelInitSpectrum = computeShader.FindKernel("InitSpectrum");
+        kernelUpdatePhase = computeShader.FindKernel("UpdatePhase");
+        kernelFFT1D_Horizontal = computeShader.FindKernel("FFT1D_Horizontal");
+        kernelFFT1D_Vertical = computeShader.FindKernel("FFT1D_Vertical");
+        kernelIFFT1D_Horizontal = computeShader.FindKernel("IFFT1D_Horizontal");
+        kernelIFFT1D_Vertical = computeShader.FindKernel("IFFT1D_Vertical");
+        kernelFinalOutput = computeShader.FindKernel("FinalOutput");
+    }
     
     // Start is called before the first frame update
     void Start()
     {
-        spectrum = new float[resolution, resolution];
-        spectrumTexture = new Texture2D(resolution, resolution);
+        tempBuffer = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.ARGBFloat)
+        {
+            enableRandomWrite = true
+        };
+        tempBuffer.Create();
+
+        heightMap = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.RFloat)
+        {
+            enableRandomWrite = true
+        };
+        heightMap.Create();
+
+        computeShader.SetInt("_Resolution", resolution);
+        computeShader.SetTexture(kernelInitSpectrum, "_TempBuffer", tempBuffer);
+        computeShader.Dispatch(kernelInitSpectrum, threadGroupX, threadGroupY, 1);
+        
+        // spectrum = new float[resolution, resolution];
+        // spectrumTexture = new Texture2D(resolution, resolution);
+        
         //InitializeSpectrum();
     }
 
     // Update is called once per frame
     void Update()
     {
-        InitializeSpectrum();
-        GenerateSpectrum();
-        PerformFFT();
+        computeShader.SetInt("_Resolution", resolution);
+        computeShader.SetFloat("_Time", Time.time * speed);
+        
+        computeShader.SetTexture(kernelInitSpectrum, "_TempBuffer", tempBuffer);
+        computeShader.SetTexture(kernelUpdatePhase, "_TempBuffer", tempBuffer);
+        computeShader.SetTexture(kernelFFT1D_Horizontal, "_TempBuffer", tempBuffer);
+        computeShader.SetTexture(kernelFFT1D_Vertical, "_TempBuffer", tempBuffer);
+        computeShader.SetTexture(kernelIFFT1D_Horizontal, "_TempBuffer", tempBuffer);
+        computeShader.SetTexture(kernelIFFT1D_Vertical, "_TempBuffer", tempBuffer);
+        computeShader.SetTexture(kernelFinalOutput, "_TempBuffer", tempBuffer);
+        computeShader.SetTexture(kernelFinalOutput, "_HeightMap", heightMap);
+
+
+        computeShader.Dispatch(kernelUpdatePhase, threadGroupX, threadGroupY, 1);
+        computeShader.Dispatch(kernelFFT1D_Horizontal, threadGroupX, threadGroupY, 1);
+        computeShader.Dispatch(kernelFFT1D_Vertical, threadGroupX, threadGroupY, 1);
+        computeShader.Dispatch(kernelIFFT1D_Horizontal, threadGroupX, threadGroupY, 1);
+        computeShader.Dispatch(kernelIFFT1D_Vertical, threadGroupX, threadGroupY, 1);
+        
+        computeShader.Dispatch(kernelFinalOutput, threadGroupX, threadGroupY, 1);
+        
+        spectrumPlaneMR.material.SetTexture("_MainTex", heightMap);
+        
+        RenderTexture.active = heightMap;
+        
+        Texture2D tempTex = new Texture2D(resolution, resolution, TextureFormat.RFloat, false);
+        tempTex.ReadPixels(new Rect(0, 0, resolution, resolution), 0, 0);
+        tempTex.Apply();
+        
+        float debugValue = tempTex.GetPixel(0, 0).r; 
+        Debug.Log($"Pixel (0,0) Value: {debugValue}");
     }
     
     private void InitializeSpectrum()
@@ -41,7 +125,28 @@ public class ShapeFFT : MonoBehaviour
         {
             for (int y = 0; y < resolution; y++)
             {
-                spectrum[x, y] = spectrum[x, y] = Mathf.Sin(frequency * x + Time.time) * Mathf.Cos(frequency * y + Time.time); 
+                // wave vectors
+                float kx = (x - resolution / 2) * 2.0f * Mathf.PI / resolution;
+                float kz = (y - resolution / 2) * 2.0f * Mathf.PI / resolution;
+                float k = Mathf.Sqrt(kx * kx + kz * kz);
+
+                if (k == 0) continue;
+                
+                // wind direction
+                Vector2 windDir = new Vector2(1, 0);
+                float windFactor = Vector2.Dot(new Vector2(kx, kz).normalized, windDir);
+                
+                // Phillips Spectrum
+                float L = windSpeed * windSpeed / g;
+                float damping = Mathf.Exp(-1.0f / (k * L * k * L));
+                float phillips = A * damping / (k * k * k * k) * windFactor * windFactor; 
+                
+                // Time Factor
+                float omega = Mathf.Sqrt(g * k);
+                float phase = omega * Time.time;
+                float realPart = Mathf.Sqrt(phillips) * Mathf.Cos(phase);
+                
+                spectrum[x, y] = realPart; 
             }
         }
 
@@ -79,18 +184,37 @@ public class ShapeFFT : MonoBehaviour
 
     private void PerformFFT()
     {
-        Complex[,] fftOutput = FFT2D(spectrum);
+        fftOutput = FFT2D(spectrum);
         
-        int size = fftOutput.GetLength(0);
-
+        // int size = fftOutput.GetLength(0);
+        //
+        // for (int x = 0; x < size; x++)
+        // {
+        //     for (int y = 0; y < size; y++)
+        //     {
+        //         float magnitude = (float) fftOutput[x, y].Magnitude;
+        //         magnitude = Mathf.Clamp01(magnitude / 10f);
+        //         
+        //         spectrumTexture.SetPixel(x, y, new Color(magnitude, magnitude, magnitude, 1.0f));
+        //     }
+        // }
+        //
+        // spectrumTexture.Apply();
+    }
+    
+    private void PerformIFFT()
+    {
+        Complex[,] ifftOutput = IFFT2D(fftOutput);
+        
+        int size = ifftOutput.GetLength(0);
         for (int x = 0; x < size; x++)
         {
             for (int y = 0; y < size; y++)
             {
-                float magnitude = (float) fftOutput[x, y].Magnitude;
-                magnitude = Mathf.Clamp01(magnitude / 10f);
+                float height = (float)ifftOutput[x, y].Real; 
+                height = Mathf.Clamp01((height + 1f) / 2f);
                 
-                spectrumTexture.SetPixel(x, y, new Color(magnitude, magnitude, magnitude, 1.0f));
+                spectrumTexture.SetPixel(x, y, new Color(height, height, height, 1.0f));
             }
         }
         
@@ -178,5 +302,77 @@ public class ShapeFFT : MonoBehaviour
         return complexData;
     }
     
+    #endregion
+
+    #region Inverse FFT 1D
+    public static Complex[] IFFT1D(Complex[] input)
+    {
+        int N = input.Length;
+        if (N <= 1) return input;
+
+        var even = new Complex[N / 2];
+        var odd = new Complex[N / 2];
+        for (int i = 0; i < N / 2; i++)
+        {
+            even[i] = input[i * 2];
+            odd[i] = input[i * 2 + 1];
+        }
+
+        var ifftEven = IFFT1D(even);
+        var ifftOdd = IFFT1D(odd);
+
+        var result = new Complex[N];
+        for (int k = 0; k < N / 2; k++)
+        {
+            Complex t = Complex.FromPolarCoordinates(1.0, +2.0 * Math.PI * k / N) * ifftOdd[k];
+            result[k] = (ifftEven[k] + t) / 2.0;
+            result[k + N / 2] = (ifftEven[k] - t) / 2.0;
+        }
+
+        return result;
+    }
+
+    #endregion
+    
+    #region Inverse FFT 2D
+    private Complex[,] IFFT2D(Complex[,] input)
+    {
+        int size = input.GetLength(0);
+        Complex[,] complexData = new Complex[size, size];
+
+        // Inverse 1D FFT each row
+        for (int y = 0; y < size; y++)
+        {
+            Complex[] row = new Complex[size];
+            for (int x = 0; x < size; x++)
+            {
+                row[x] = input[x, y];
+            }
+
+            Complex[] ifftRow = IFFT1D(row);
+            for (int x = 0; x < size; x++)
+            {
+                complexData[x, y] = ifftRow[x];
+            }
+        }
+
+        // Inverse 1D FFT each column
+        for (int x = 0; x < size; x++)
+        {
+            Complex[] column = new Complex[size];
+            for (int y = 0; y < size; y++)
+            {
+                column[y] = complexData[x, y];
+            }
+
+            Complex[] ifftColumn = IFFT1D(column);
+            for (int y = 0; y < size; y++)
+            {
+                complexData[x, y] = ifftColumn[y];
+            }
+        }
+
+        return complexData;
+    }
     #endregion
 } 
--- Assets/Shaders/FFT/SpectrumTexture.shader
+++ Assets/Shaders/FFT/SpectrumTexture.shader
@@ -1,50 +1,74 @@
-Shader "Unlit/SpectrumTexture"
+Shader "Ocean/HeightMapWithLighting"
 {
     Properties
     {
-        _MainTex ("Texture", 2D) = "white" {}
+        _MainTex("Height Map", 2D) = "white" {}  
+        _Amplitude("Amplitude", Float) = 1.0     
+        _Color("Base Color", Color) = (0.4, 0.6, 1, 1) 
     }
+
     SubShader
     {
-        Tags { "RenderType"="Opaque" }
-        LOD 100
-
+        Tags { "RenderType" = "Opaque" }
         Pass
         {
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
+            #pragma multi_compile_fwdbase
+
+            #include <UnityLightingCommon.cginc>
 
             #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            float _Amplitude;
+            fixed4 _Color;
 
             struct appdata
             {
                 float4 vertex : POSITION;
                 float2 uv : TEXCOORD0;
+                float3 normal : NORMAL;
             };
 
             struct v2f
             {
+                float4 pos : SV_POSITION;
                 float2 uv : TEXCOORD0;
-                float4 vertex : SV_POSITION;
+                float3 worldNormal : NORMAL;
+                float3 worldPos : TEXCOORD1;
             };
-
-            sampler2D _MainTex;
-            float4 _MainTex_ST;
-
-            v2f vert (appdata v)
+            
+            v2f vert(appdata v)
             {
                 v2f o;
-                o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.uv = v.uv;
+                
+                float height = tex2Dlod(_MainTex, float4(v.uv, 0, 0)).r * _Amplitude;
+                float3 displacedVertex = v.vertex.xyz;
+                displacedVertex.y += height; 
+
+                o.pos = UnityObjectToClipPos(float4(displacedVertex, 1.0));
+                o.worldPos = mul(unity_ObjectToWorld, float4(displacedVertex, 1.0)).xyz;
+                o.worldNormal = UnityObjectToWorldNormal(v.normal);
                 return o;
             }
-
-            fixed4 frag (v2f i) : SV_Target
+            
+            fixed4 frag(v2f i) : SV_Target
             {
-                // sample the texture
-                fixed4 col = tex2D(_MainTex, i.uv);
-                return col;
+                fixed3 worldNormal = normalize(i.worldNormal);
+                fixed3 lightDir = normalize(_WorldSpaceLightPos0.xyz); 
+                fixed3 viewDir = normalize(UnityWorldSpaceViewDir(i.worldPos));
+                
+                fixed3 diffuse = max(0, dot(worldNormal, lightDir)) * _LightColor0.rgb;
+                fixed3 ambient = UNITY_LIGHTMODEL_AMBIENT.rgb;
+                fixed3 halfDir = normalize(lightDir + viewDir);
+                float specular = pow(max(0, dot(worldNormal, halfDir)), 16.0);
+
+                fixed3 lighting = ambient + diffuse + specular;
+
+                return fixed4(_Color.rgb * lighting, 1.0);
             }
             ENDCG
         }
