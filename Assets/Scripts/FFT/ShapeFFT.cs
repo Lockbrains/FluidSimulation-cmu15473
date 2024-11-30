55a7ae1d2c17b710262926582450cecdce273010
Lingheng Tao <40527691+Lockbrains@users.noreply.github.com>
2024-11-30 03:42:12 -0500

    FFT Water Shape Finished!

--- Assets/Scripts/FFT/ShapeFFT.cs
+++ Assets/Scripts/FFT/ShapeFFT.cs
@@ -13,6 +13,7 @@ using Vector2 = UnityEngine.Vector2;
 public class ShapeFFT : MonoBehaviour
 {
     [SerializeField] private int N = 256; // N
+    float log2N => Mathf.Log(N, 2);
     [SerializeField] private float L = 1000f; // Patch Size
     [SerializeField] private float A = 0.01f; // Adjustment
     [SerializeField] private float V = 40f; // Wind Speed
@@ -23,70 +24,166 @@ public class ShapeFFT : MonoBehaviour
     [SerializeField] private MeshRenderer h0kRenderer;
     [SerializeField] private MeshRenderer h0MinusKRenderer;
     [SerializeField] private MeshRenderer butterflyTextureRenderer;
-    [SerializeField] private MeshRenderer updateRenderer;
+    [SerializeField] private MeshRenderer hktDyRenderer, hktDxRenderer, hktDzRenderer;
+    [SerializeField] private MeshRenderer pingpongRenderer0, pingpongRenderer1;
+    [SerializeField] private MeshRenderer pingpongRendererX0, pingpongRendererX1;
+    [SerializeField] private MeshRenderer pingpongRendererZ0, pingpongRendererZ1;
     [SerializeField] private MeshRenderer FFT1DResultTexture;
+    [SerializeField] private MeshRenderer FFTResultRenderer;
     
+    [Header("For Water Rendering")]
+    [SerializeField] private MeshRenderer watermeshRenderer;
+
+    [Header("Compute Shaders")]
+    [SerializeField] private bool useGPU = true;
+    [SerializeField] private ComputeShader fftInitShader;
+    [SerializeField] private ComputeShader fourierComponentShader;
+    [SerializeField] private ComputeShader butterflyTextureShader;
+    [SerializeField] private ComputeShader butterflyComputeShader;
+    [SerializeField] private ComputeShader inversionComputeShader;
     // Private Variables 
     private readonly float gravity = 9.81f;
-
+    
+    // For CPU
     private Complex[,] h0k;
     private Complex[,] h0MinusK;
 
     private Texture2D h0kTexture;
     private Texture2D h0MinusKTexture;
     private Texture2D butterflyTexture;
-
+    
     private Texture2D pingpong0, pingpong1;
+    
+    // For GPU
+    private int[] bitReversedIndices;
+    
+    private RenderTexture h0kRenderTexture;
+    private RenderTexture h0MinusKRenderTexture;
+    private RenderTexture butterflyRenderTexture;
+    private RenderTexture pingpong0RT_Dx, pingpong1RT_Dx;
+    private RenderTexture pingpong0RT_Dy, pingpong1RT_Dy;
+    private RenderTexture pingpong0RT_Dz, pingpong1RT_Dz;
+    private RenderTexture displacementRT;
+    
+    private RenderTexture hktDy, hktDx, hktDz;
 
+    private int hktHandle;
+    private int butterflyTextureHandle;
+    private int butterflyComputeHandle;
+    private int inversionHandle;
+    
     private float time = 0f;
 
     void Start()
     {
-        InitializeSpectrum();
+        if (useGPU)
+        {
+            InitializeRenderTextures();
+            InitializeSpectrumOnGPU();
+            InitializeOtherComputeShaders();
+        }
+        else
+        {
+            InitializeSpectrum();
 
-        int[] bitReversedIndices = GenerateBitReversedIndices(N);
-        InitializeButterflyTexture(N, bitReversedIndices);
+            int[] bitReversedIndices = GenerateBitReversedIndices(N);
+            InitializeButterflyTexture(N, bitReversedIndices);
 
-        InitializePingPongTextures(N);
+            InitializePingPongTextures(N);
         
-        butterflyTextureRenderer.material.SetTexture("_MainTex", butterflyTexture);
-        butterflyTexture.filterMode = FilterMode.Point;
-        //butterflyTexture.wrapMode = TextureWrapMode.Repeat;
-        //butterflyTextureRenderer.material.SetTextureScale("_MainTex", new Vector2(1.0f, 1.0f / (Mathf.Log(N, 2))));
+            butterflyTextureRenderer.material.SetTexture("_MainTex", butterflyTexture);
+            butterflyTexture.filterMode = FilterMode.Point;
         
-        h0kTexture = GenerateTexture(h0k);
-        h0kTexture.filterMode = FilterMode.Point;
+            h0kTexture = GenerateTexture(h0k);
+            h0kTexture.filterMode = FilterMode.Point;
         
-        h0MinusKTexture = GenerateTexture(h0MinusK);
-        h0MinusKTexture.filterMode = FilterMode.Point; 
+            h0MinusKTexture = GenerateTexture(h0MinusK);
+            h0MinusKTexture.filterMode = FilterMode.Point; 
         
-        h0kRenderer.material.SetTexture("_MainTex", h0kTexture);
-        h0MinusKRenderer.material.SetTexture("_MainTex", h0MinusKTexture);
+            h0kRenderer.material.SetTexture("_MainTex", h0kTexture);
+            h0MinusKRenderer.material.SetTexture("_MainTex", h0MinusKTexture);
+        }
     }
     // Update is called once per frame
     void Update()
     {
-        // Update Fourier Components
+        int currentPingpong = 0;
         time += Time.deltaTime;
 
-        Texture2D hktTexture = UpdateFourierComponents(h0k, h0MinusK, time);
-        
-        CopyTextureData(hktTexture, pingpong1);
+        if (useGPU)
+        {
+            // Update Fourier Components
+            fourierComponentShader.SetFloat("Time", time);
 
-        //updateRenderer.material.SetTexture("_MainTex", hktTexture); 
-        
-        //Complex[,] hktData = ExtractDataFromTexture(hktTexture);
-        
-        
-        // 执行水平 FFT
-        PerformHorizontalFFT();
+            int threadGroups = Mathf.CeilToInt(N / 16.0f);
+            fourierComponentShader.Dispatch(hktHandle, threadGroups, threadGroups, 1);
+            
+            hktDyRenderer.material.SetTexture("_MainTex", hktDy);
+            hktDzRenderer.material.SetTexture("_MainTex", hktDz);
+            hktDxRenderer.material.SetTexture("_MainTex", hktDx);
+            
+            Graphics.Blit(hktDy, pingpong0RT_Dy);
+            Graphics.Blit(hktDx, pingpong0RT_Dx);
+            Graphics.Blit(hktDz, pingpong0RT_Dz);
+            
+            // horizontal fft
+            for (int stage = 0; stage < log2N; stage++)
+            {
+                butterflyComputeShader.SetInt("stage", stage);
+                butterflyComputeShader.SetInt("pingpong", currentPingpong);
+                butterflyComputeShader.SetInt("direction", 0);
+                
+                DispatchButterflyComputeShader();
+                
+                currentPingpong = 1 - currentPingpong;
+            }
+            
+            // vertical fft
+            for (int stage = 0; stage < log2N; stage++)
+            {
+                butterflyComputeShader.SetInt("stage", stage);
+                butterflyComputeShader.SetInt("pingpong", currentPingpong);
+                butterflyComputeShader.SetInt("direction", 1);
+                
+                DispatchButterflyComputeShader();
+                
+                currentPingpong = 1 - currentPingpong;
+            }
+            
+            pingpongRenderer0.material.SetTexture("_MainTex", pingpong0RT_Dy);
+            pingpongRenderer1.material.SetTexture("_MainTex", pingpong1RT_Dy);
+            pingpongRendererX0.material.SetTexture("_MainTex", pingpong0RT_Dx);
+            pingpongRendererX1.material.SetTexture("_MainTex", pingpong1RT_Dx);
+            pingpongRendererZ0.material.SetTexture("_MainTex", pingpong0RT_Dz);
+            pingpongRendererZ1.material.SetTexture("_MainTex", pingpong1RT_Dz);
+            
+            // Inversion
+            inversionComputeShader.Dispatch(inversionHandle, threadGroups, threadGroups, 1);
+            FFTResultRenderer.material.SetTexture("_MainTex", displacementRT);
+            
+            watermeshRenderer.material.SetTexture("_DisplacementX", hktDx);
+            watermeshRenderer.material.SetTexture("_DisplacementZ", hktDz);
+            watermeshRenderer.material.SetTexture("_HeightMap", displacementRT);
+        }
+        else
+        {
+            // Update Fourier Components
+            Texture2D hktTexture = UpdateFourierComponents(h0k, h0MinusK, time);
 
-        // 执行垂直 FFT
-        PerformVerticalFFT();
-        
-        // Visualize 1D FFT Result
-        // Texture2D fftResultTexture = GenerateTexture(hktData);
-        FFT1DResultTexture.material.SetTexture("_MainTex", pingpong1);
+            CopyTextureData(hktTexture, pingpong1);
+
+            //updateRenderer.material.SetTexture("_MainTex", hktTexture); 
+
+            //Complex[,] hktData = ExtractDataFromTexture(hktTexture);
+
+            PerformHorizontalFFT();
+            //
+            // PerformVerticalFFT();
+
+            // Visualize 1D FFT Result
+            // Texture2D fftResultTexture = GenerateTexture(hktData);
+            FFT1DResultTexture.material.SetTexture("_MainTex", pingpong1);
+        }
     }
     
     #region Phase 1 Initialization
@@ -97,13 +194,35 @@ public class ShapeFFT : MonoBehaviour
         float u1 = UnityEngine.Random.value;
         float u2 = UnityEngine.Random.value;
 
+        float theta = Mathf.Sin(2.0f * Mathf.PI * u2); 
         float z0 = Mathf.Sqrt(-2.0f * Mathf.Log(u1)) * Mathf.Cos(2.0f * Mathf.PI * u2);
         float z1 = Mathf.Sqrt(-2.0f * Mathf.Log(u1)) * Mathf.Sin(2.0f * Mathf.PI * u2);
 
         return (z0, z1);
     }
     
+    // Generating Random Noise Texture for GPU 
+    private Texture2D GenerateNoiseTexture(int N)
+    {
+        Texture2D noiseTexture = new Texture2D(N, N, TextureFormat.ARGB32, false);
+        noiseTexture.filterMode = FilterMode.Point;
+        noiseTexture.wrapMode = TextureWrapMode.Repeat;
+
+        Color[] noisePixels = new Color[N * N];
+        for (int i = 0; i < noisePixels.Length; i++)
+        {
+            float u1 = UnityEngine.Random.value;
+            noisePixels[i] = new Color(u1, u1, u1, 0);
+        }
+        noiseTexture.SetPixels(noisePixels);
+        noiseTexture.Apply();
+
+        return noiseTexture;
+    }
     
+    /// <summary>
+    /// Initialize Spectrum (h0(k) and h0(-k) on CPU
+    /// </summary>
     private void InitializeSpectrum()
     {
         float windLength = V * V / gravity; // L = V^2/g;
@@ -145,6 +264,158 @@ public class ShapeFFT : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Initialize Spectrum (h0(k) and h0(-k) on GPU
+    /// </summary>
+    private void InitializeSpectrumOnGPU()
+    {
+        // Initialize h0(k) render texture
+        h0kRenderTexture = new RenderTexture(N, N, 0, RenderTextureFormat.ARGBFloat);
+        h0kRenderTexture.filterMode = FilterMode.Point; // no interpolation
+        h0kRenderTexture.enableRandomWrite = true; // writable
+        h0kRenderTexture.wrapMode = TextureWrapMode.Repeat;
+        h0kRenderTexture.Create();
+        
+        // Initialize h0(-k) render texture
+        h0MinusKRenderTexture = new RenderTexture(N, N, 0, RenderTextureFormat.ARGBFloat);
+        h0MinusKRenderTexture.filterMode = FilterMode.Point; // no interpolation
+        h0MinusKRenderTexture.enableRandomWrite = true; // writable
+        h0MinusKRenderTexture.wrapMode = TextureWrapMode.Repeat;
+        h0MinusKRenderTexture.Create();
+        
+        // find compute shader
+        int kernel = fftInitShader.FindKernel("FFTInit");
+        
+        // set compute shader params
+        fftInitShader.SetTexture(kernel, "H0k", h0kRenderTexture);
+        fftInitShader.SetTexture(kernel, "H0MinusK", h0MinusKRenderTexture);
+        
+        // pass in other params
+        fftInitShader.SetInt("N", N);
+        fftInitShader.SetFloat("L", L);
+        fftInitShader.SetFloat("A", A);
+        fftInitShader.SetFloat("V", V);
+        fftInitShader.SetFloats("windDirection", new float[] { windDirection.x, windDirection.y });
+        
+        // pass in noise textures
+        fftInitShader.SetTexture(kernel, "NoiseR0", GenerateNoiseTexture(N));
+        fftInitShader.SetTexture(kernel, "NoiseI0", GenerateNoiseTexture(N));
+        fftInitShader.SetTexture(kernel, "NoiseR1", GenerateNoiseTexture(N));
+        fftInitShader.SetTexture(kernel, "NoiseI1", GenerateNoiseTexture(N));
+        
+        // Dispatch the shader
+        int threadGroupSize = Mathf.CeilToInt(N / 16.0f);
+        fftInitShader.Dispatch(kernel, threadGroupSize, threadGroupSize, 1);
+        
+        // visualization
+        h0kRenderer.material.SetTexture("_MainTex", h0kRenderTexture);
+        h0MinusKRenderer.material.SetTexture("_MainTex", h0MinusKRenderTexture);
+    }
+
+    private RenderTexture CreateRenderTexture(int width, int height, bool interpolate = false) {
+        RenderTexture rt = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat);
+        if (!interpolate)
+        {
+            rt.filterMode = FilterMode.Point;
+        }
+        else
+        {
+            rt.filterMode = FilterMode.Bilinear;
+        }
+        rt.enableRandomWrite = true;
+        rt.Create();
+        return rt;
+    }
+    private void InitializeRenderTextures()
+    {
+        hktDy = CreateRenderTexture(N, N);
+        hktDx = CreateRenderTexture(N, N);
+        hktDz = CreateRenderTexture(N, N);
+        
+        int stages = Mathf.RoundToInt(Mathf.Log(N, 2)); 
+        butterflyRenderTexture = new RenderTexture(stages, N,0, RenderTextureFormat.ARGBFloat);
+        butterflyRenderTexture.filterMode = FilterMode.Point;
+        butterflyRenderTexture.enableRandomWrite = true;
+        butterflyRenderTexture.Create();
+        
+        pingpong0RT_Dy = CreateRenderTexture(N, N);
+        pingpong1RT_Dy = CreateRenderTexture(N, N);
+        pingpong0RT_Dx = CreateRenderTexture(N, N);
+        pingpong1RT_Dx = CreateRenderTexture(N, N);
+        pingpong0RT_Dz = CreateRenderTexture(N, N);
+        pingpong1RT_Dz = CreateRenderTexture(N, N);
+        
+        displacementRT = CreateRenderTexture(N, N, true);
+    }
+
+    private void InitializeOtherComputeShaders()
+    {
+        hktHandle = fourierComponentShader.FindKernel("FourierComponent");
+        
+        // hkt 
+        fourierComponentShader.SetTexture(hktHandle, "TildeHktDy", hktDy);
+        fourierComponentShader.SetTexture(hktHandle, "TildeHktDx", hktDx);
+        fourierComponentShader.SetTexture(hktHandle, "TildeHktDz", hktDz);
+        
+        fourierComponentShader.SetTexture(hktHandle, "Tilde_h0k", h0kRenderTexture);
+        fourierComponentShader.SetTexture(hktHandle, "Tilde_h0MinusK", h0MinusKRenderTexture);
+        
+        fourierComponentShader.SetInt("N", N);
+        fourierComponentShader.SetFloat("L", L);
+        
+        fourierComponentShader.SetFloat("Time", time);
+
+        int threadGroups = Mathf.CeilToInt(N / 16.0f);
+        fourierComponentShader.Dispatch(hktHandle, threadGroups, threadGroups, 1);
+            
+        hktDyRenderer.material.SetTexture("_MainTex", hktDy);
+        hktDzRenderer.material.SetTexture("_MainTex", hktDz);
+        hktDxRenderer.material.SetTexture("_MainTex", hktDx);
+        
+        Graphics.Blit(hktDy, pingpong0RT_Dy);
+        Graphics.Blit(hktDx, pingpong0RT_Dx);
+        Graphics.Blit(hktDz, pingpong0RT_Dz);
+        
+        // Butterfly Texture
+        bitReversedIndices = GenerateBitReversedIndices(N);
+        
+        butterflyTextureHandle = butterflyTextureShader.FindKernel("ButterflyTexture");
+        butterflyTextureShader.SetTexture(butterflyTextureHandle, "butterflyTexture", butterflyRenderTexture);
+        butterflyTextureShader.SetInt("N", N);
+        
+        ComputeBuffer bitReversedBuffer = new ComputeBuffer(bitReversedIndices.Length, sizeof(int));
+        bitReversedBuffer.SetData(bitReversedIndices);
+        butterflyTextureShader.SetBuffer(butterflyTextureHandle, "bit_reversed", bitReversedBuffer);
+        
+        int threadGroupY = Mathf.CeilToInt(N / 16.0f);
+        int threadGroupX = Mathf.CeilToInt(Mathf.Log(N, 2) / 16.0f);
+        butterflyTextureShader.Dispatch(butterflyTextureHandle, threadGroupX, threadGroupY, 1);
+        
+        bitReversedBuffer.Release();
+        
+        butterflyTextureRenderer.material.SetTexture("_MainTex", butterflyRenderTexture);
+        
+        // Butterfly Compute 
+        butterflyComputeHandle = butterflyComputeShader.FindKernel("Butterfly");
+        butterflyComputeShader.SetTexture(butterflyComputeHandle, "butterflyTexture", butterflyRenderTexture);
+        butterflyComputeShader.SetTexture(butterflyComputeHandle, "pingpong0_Dy", pingpong0RT_Dy);  
+        butterflyComputeShader.SetTexture(butterflyComputeHandle, "pingpong1_Dy", pingpong1RT_Dy);  
+        butterflyComputeShader.SetTexture(butterflyComputeHandle, "pingpong0_Dx", pingpong0RT_Dx);  
+        butterflyComputeShader.SetTexture(butterflyComputeHandle, "pingpong1_Dx", pingpong1RT_Dx);
+        butterflyComputeShader.SetTexture(butterflyComputeHandle, "pingpong0_Dz", pingpong0RT_Dz);  
+        butterflyComputeShader.SetTexture(butterflyComputeHandle, "pingpong1_Dz", pingpong1RT_Dz);
+        
+        // Inversion
+        inversionHandle = inversionComputeShader.FindKernel("IP");
+        inversionComputeShader.SetInt("N", N);
+        inversionComputeShader.SetTexture(inversionHandle, "pingpong0_Dy", pingpong0RT_Dy);
+        inversionComputeShader.SetTexture(inversionHandle, "pingpong1_Dy", pingpong1RT_Dy);
+        inversionComputeShader.SetTexture(inversionHandle, "pingpong0_Dx", pingpong0RT_Dx);
+        inversionComputeShader.SetTexture(inversionHandle, "pingpong1_Dx", pingpong1RT_Dx);
+        inversionComputeShader.SetTexture(inversionHandle, "pingpong0_Dz", pingpong0RT_Dz);
+        inversionComputeShader.SetTexture(inversionHandle, "pingpong1_Dz", pingpong1RT_Dz);
+        inversionComputeShader.SetTexture(inversionHandle, "displacement", displacementRT);
+    }
     private int[] GenerateBitReversedIndices(int N)
     {
         int[] indices = new int[N];
@@ -326,6 +597,11 @@ public class ShapeFFT : MonoBehaviour
     #endregion
     
     #region Phase 3 Horitontal 1D FFT
+    private void DispatchButterflyComputeShader() {
+        int threadGroups = Mathf.CeilToInt(N / 16.0f);
+        butterflyComputeShader.Dispatch(butterflyComputeHandle, threadGroups, threadGroups, 1);
+    }
+    
     private Complex[,] ExtractDataFromTexture(Texture2D texture)
     {
         int N = texture.width;
---
+++ Assets/Shaders/FFT/WaterbodyFFT.shader
@@ -0,0 +1,107 @@
+Shader "FluidSimulation/VertexDisplacement"
+{
+    Properties
+    {
+        _HeightMap("Height Map", 2D) = "white" {} 
+        _DisplacementScale("Displacement Scale", Float) = 1.0
+        _MainTex("Main Texture", 2D) = "white" {}
+        _LightColor("Light Color", Color) = (1,1,1,1)
+        _AmbientColor("Ambient Color", Color) = (0.2, 0.2, 0.2, 1) 
+        _SpecularPower("Specular Power", Float) = 16.0
+        _ChoppyFactor("Choppy Factor", Float) = 0.2
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            
+            sampler2D _HeightMap;
+            sampler2D _MainTex;
+            float _DisplacementScale;
+            float4 _LightColor;
+            float4 _AmbientColor;
+            float _ChoppyFactor;
+            float _SpecularPower;
+            
+            float4x4 _ObjectToWorld;
+            float4x4 _WorldToObject;
+            float4x4 _UnityMatrixVP;
+
+            struct appdata
+            {
+                float4 vertex : POSITION; 
+                float2 uv : TEXCOORD0;  
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float3 worldPos: TEXCOORD1;
+                float2 uv : TEXCOORD0;       
+            };
+
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.uv = v.uv;
+                
+                float height = tex2Dlod(_HeightMap, float4(v.uv, 0, 0)).g;
+                float dx = tex2Dlod(_HeightMap, float4(v.uv, 0, 0)).r;
+                float dz = tex2Dlod(_HeightMap, float4(v.uv, 0, 0)).b;
+                
+                float3 newPos = v.vertex.xyz;
+                newPos.y += height * _DisplacementScale;
+                newPos.x += dx * _ChoppyFactor;
+                newPos.z += dz * _ChoppyFactor;
+
+                float3 worldPos = UnityObjectToWorldDir(newPos);
+                
+                o.vertex = UnityObjectToClipPos(newPos);
+                o.worldPos = worldPos;
+
+                return o;
+            }
+            
+            float4 frag(v2f i) : SV_Target
+            {
+                float3 ambient = _AmbientColor.rgb;
+                
+                float3 lightDir = normalize(float3(0, 1, -1));
+                
+                float heightL = tex2D(_HeightMap, i.uv + float2(-0.01, 0)).g;
+                float heightR = tex2D(_HeightMap, i.uv + float2(0.01, 0)).g;
+                float heightD = tex2D(_HeightMap, i.uv + float2(0, -0.01)).g;
+                float heightU = tex2D(_HeightMap, i.uv + float2(0, 0.01)).g;
+
+                float3 normal = normalize(float3(heightL - heightR, 2.0, heightD - heightU));
+                
+                float diff = max(0.0, dot(normal, lightDir));
+                float3 diffuse = diff * _LightColor.rgb;
+                
+                float3 viewDir = normalize(-i.worldPos); 
+                float3 reflectDir = reflect(-lightDir, normal);
+                float spec = pow(max(dot(viewDir, reflectDir), 0.0), _SpecularPower);
+                float3 specular = spec * _LightColor.rgb;
+                
+                float3 baseColor = tex2D(_MainTex, i.uv).rgb;
+                
+                float3 finalColor = ambient + diffuse + specular;
+                finalColor *= baseColor;
+
+                return float4(finalColor, 1.0);
+            }
+            ENDCG
+        }
+    }
+    FallBack "Diffuse"
+}
